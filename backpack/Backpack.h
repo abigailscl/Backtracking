36c8939d35b8d1edcd0192d2dcdfed3caf412cbc
CharlRomero <romeokarlos12@gmail.com>
2020-07-28 22:29:19 -0500

    documentation added

--- backpack/Backpack.h
+++ backpack/Backpack.h
@@ -1,7 +1,15 @@
+/******************************************************************
+*            UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE              *
+* CARRERA: Ingenieria de Software                                 *
+* SEMESTRE: Tercer semestre                                       *
+* MATERIA: Estructura de Datos                                    *
+* NRC: 6396                                                       *
+******************************************************************/
 #ifndef BACKPACK_H
 #define BACKPACK_H
-
+///@author Abigail Cabascango and Carlos Romero M.
 #include "Object.h"
+#include "InputData.h"
 
 class Backpack
 {
@@ -11,19 +19,42 @@ private:
     int* solution;
     int numObject;
 public:
+    /**
+    *@brief Default constructor
+    *@param int queens number.
+    */
     Backpack () {}
+    /**
+    *@brief Constructor
+    *@param int max weigth.
+    *@param int number object.
+    */
     Backpack (int maxWeigth, int numObject)
     {
         this->numObject = numObject;
         this->maxWeigth = maxWeigth;
         this->object = new Object[numObject];
     }
+    ///@brief Destroyer
     ~Backpack () {}
+    /**
+    *@brief start the solutions.
+    *@param void.
+    *@return void.
+    */
     void findSolution()
     {
         backpackSolution(0, maxWeigth, 0, 0);
     }
-    void backpackSolution(int i, int r, int solution, int optimal)
+    /**
+    *@brief find the solutions.
+    *@param i iterator inialized with zero.
+    *@param weigth backpack.
+    *@param solution is the biggest benefit.
+    *@param optimal is the optimal benefit.
+    *@return void.
+    */
+    void backpackSolution(int i, int weigth, int solution, int optimal)
     {
         int k;
         if (optimal > solution)
@@ -36,9 +67,9 @@ public:
 
             for (k = i; k < numObject; k++)
             {
-                if ((object + k)->getWeigth() <= r)
+                if ((object + k)->getWeigth() <= weigth)
                 {
-                    backpackSolution(k, (r - (object + k)->getWeigth()), (solution + (object + k)->getValue()), optimal);
+                    backpackSolution(k, (weigth - (object + k)->getWeigth()), (solution + (object + k)->getValue()), optimal);
                     if ((solution + object->getValue()) > optimal)
                     {
                         optimal = solution + (object + k)->getValue();
@@ -48,21 +79,34 @@ public:
         }
 
     }
+    /**
+    *@brief Fill the backpack.
+    *@param void.
+    *@return void.
+    */
     void fillBackpack()
     {
         int weigth, value;
         Object ob;
+        InputData<int> enter;
         for (int i = 0; i < numObject; i++)
         {
-            std::cout << "Ingresar peso al objecto[" << (i + 1) << "]: " << std::endl;
-            std::cin >> weigth;
-            std::cout << "Ingresar valor al objecto[" << (i + 1) << "]: " << std::endl;
-            std::cin >> value;
+            std::cout << "[" << (i + 1) << "]";
+            weigth = atoi(enter.positiveInteger("Ingrese el peso del objeto: ").c_str());
+            
+            std::cout << "[" << (i + 1) << "]";
+            value = atoi(enter.positiveInteger("Ingrese el valor del objeto: ").c_str());
+            
             ob.setWeigth(weigth);
             ob.setValue(value);
             *(object + i) = ob;
         }
     }
+    /**
+    *@brief Print the objects.
+    *@param void.
+    *@return void.
+    */
     void printObject()
     {
         for (int i = 0; i < numObject; i++)
@@ -70,22 +114,41 @@ public:
             std::cout << "Peso: " << (object + i)->getWeigth() << ", Valor: " << (object + i)->getValue() << std::endl;
         }
     }
+    /**
+    *@brief get the max weigth of backpack.
+    *@param void.
+    *@return void.
+    */
     int getMaxWeigth()
     {
         return maxWeigth;
     }
+    /**
+    *@brief set the max weigth of backpack.
+    *@param int max weigth.
+    *@return void.
+    */
     void setMaxWeigth(int maxWeigth)
     {
         this->maxWeigth = maxWeigth;
     }
+    /**
+    *@brief get the object of backpack.
+    *@param void.
+    *@return void.
+    */
     Object* getObject()
     {
         return object;
     }
+    /**
+    *@brief set the object of backpack.
+    *@param Object* object.
+    *@return void.
+    */
     void setObject(Object* object)
     {
         this->object = object;
     }
 };
-
 #endif // BACKPACK_H
